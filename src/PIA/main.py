91455251a30a947cea72dba4cde70620e508b1db
aserranoo <xas_serranox@hotmail.com>
2017-05-31 22:39:41 -0500

    Se termina rugosidad

--- src/PIA/main.py
+++ src/PIA/main.py
@@ -7,19 +7,19 @@ import tkFileDialog
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 def Rugosidad():
-    images = []    
+    images = []
     for item in Org_img:        
-        blur = cv2.bilateralFilter(item,9,75,75)
+        blur = cv2.medianBlur(item,5)
         ret, otsu = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        images.append([blur, otsu])
-    return images
-    
+        images = [blur, otsu]
+    return images  
 #Inicia programa
 print "\n[---- Rugosidad Arbol ----] \n\n (i) - Vamos a procesar una imagen primero escoje un archivo de imagen valido "
 
 #Seleccion de imagen
-# Tkinter.Tk().withdraw() 
+Tkinter.Tk().withdraw()
 ruta = tkFileDialog.askopenfilenames(filetypes=(("Archivos JPG", "*.jpg"),
 											("Archivos JPEG", "*.jpeg"),
 											("Archivos PNG", "*.png"),
@@ -30,40 +30,36 @@ try:
     count = 0    
     for item in ruta:
         Org_img.append(cv2.imread(item, 0))
-    # print Org_img
-    # Or_img 	= cv2.imread(ruta,0) #Imagen original
-    # Pr_img	= cv2.imread(ruta,0) #Imagen procesada
-    # prop 	= Or_img.shape   #obtiene propiedades de la imagen
-    # height 	= prop[0] - 1 #obtiene filas
-    # width 	= prop[1] - 1 #obtiene cols	
-    # print "\n\n (i) - Imagen leida exitosamente.\n       Detalles:\n       -Ancho  -> " + str(width) + "px\n       -Altura -> " + str(height) + "px\n       -Ruta   -> " + ruta 
-
-    #Se realiza la opcion seleccionada
-    try:	                      
-        images = Rugosidad()
-        hists = []
-        res = []        
-        for item in images:
-            count = 0    
-            for i in item:                
-                if count == 0:
-                    hist = cv2.calcHist([i.ravel()],[0],None,[256],[0,256])
-                    hists.append(hist)
-                count+=1
-        for i in range(1, len(hists)):
-            res.append(cv2.compareHist(hists[0], hists[i], cv2.HISTCMP_BHATTACHARYYA))
+    try:
+        bordes = 0
+        rug = 0
+        total = 0
+        images = Rugosidad()                            
+        titles = ['Filtro Mediana','Histograma',"Umbral Otsu"]
         
-        # titles = ['Filtro Bilateral','Histograma',"Umbral Otsu"]            
-        # for i in range(0, len(images)):            
-        #     plt.subplot(1,3,1),plt.imshow(images[0],'gray')
-        #     plt.title(titles[0]), plt.xticks([]), plt.yticks([])
-        #     plt.subplot(1,3,2),plt.hist(images[0].ravel(), 256)
-        #     plt.title(titles[1]), plt.xticks([]), plt.yticks([])
-        #     plt.subplot(1,3,3),plt.imshow(images[1],'gray')
-        #     plt.title(titles[2]), plt.xticks([]), plt.yticks([])
-        # plt.show() 
-        # Guardar()
-    except Exception as e:        
+        edges = cv2.Canny(images[1],600,50)
+        prop = edges.shape
+        height = prop[0]
+        width = prop[1]
+        for y in range(height):
+            total +=  1
+            for x in range(width):
+                total +=  1
+                if edges[y,x] > 0:                    
+                    bordes += 1             
+        rug =  total/bordes
+        plt.subplot(2,4,1),plt.imshow(images[0],cmap = 'gray')
+        plt.title('Imagen Original'), plt.xticks([]), plt.yticks([])        
+        plt.subplot(2,4,2), plt.hist(images[0].ravel(),256)
+        plt.title('Histogramas'), plt.xticks([], plt.yticks([]))
+        plt.subplot(2,4,3),plt.imshow(edges,cmap = 'gray')
+        plt.title('Bordes'), plt.xticks([]), plt.yticks([])
+        plt.subplot(2,4,4),plt.imshow(images[1],cmap = 'gray')
+        plt.title('Umbral'), plt.xticks([]), plt.yticks([])              
+        plt.subplot(2,4,5), plt.axis('off')
+        plt.title('Rugosidad = ' + str(100-rug) + '\n'), plt.xticks([]), plt.yticks([]), plt.axis('off')   
+        plt.show()        
+    except Exception as e:
         print e
         print("\n\n(X) - Esa opcion no es valida")
 
